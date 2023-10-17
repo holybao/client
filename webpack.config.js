91138694ea6807d0b7ebb519e917fd172133a5bf
libaohe <libaohe@tencent.com>
2023-10-17 18:28:07 +0800

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,12 @@ module.exports = {
     historyApiFallback: true,
   },
 
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: ['.ts', '.tsx', '...'],
